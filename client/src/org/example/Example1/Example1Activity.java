cdf9c371e51244a34dcf788c562ee3e1fb4a836b
robi-y <robi@post.jce.ac.il>
2011-12-05 10:49:39 +0200

    added project files

---
+++ client/src/org/example/Example1/Example1Activity.java
@@ -0,0 +1,13 @@
+package org.example.Example1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Example1Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
